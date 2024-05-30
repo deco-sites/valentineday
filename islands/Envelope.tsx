ed3e9487394fa8e23c7cfaaffe5483531c1419d8
Luan Lemos <ipsluan@icloud.com>
2024-05-30 16:20:12 -0300

    feat: add animation after click envelope

--- islands/Envelope.tsx
+++ islands/Envelope.tsx
@@ -1,23 +1,28 @@
+import { asset } from "$fresh/runtime.ts";
 import { useSignal } from "@preact/signals";
 
 export function Envelope() {
   const envelopeOpen = useSignal(false);
+  const confetti = asset("/js/createConfetti.js");
 
   const handleClick = () => {
     if (envelopeOpen.value) return;
 
     const audio = new Audio("/music.mp3");
-    audio.play();
+    // audio.play();
 
     envelopeOpen.value = true;
   };
 
   return (
     <>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/gsap.min.js">
+      <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.9.3/dist/confetti.browser.min.js">
       </script>
 
+      <script src={confetti} type="text/javascript" defer></script>
+
       <button
+        id="envelope"
         onClick={handleClick}
         class={`my-4 ${envelopeOpen.value ? "animate-none" : "animate-pulse"}`}
       >
